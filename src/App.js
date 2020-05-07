1bad7b75775fe92c624848dac0ce5972c8552a73
Boah Lee <boahlee@hotmail.com>
2020-05-08 09:44:27 +1000

    Upload to github

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Havanese"),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { class: "whatever" }, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Cockatiel",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
