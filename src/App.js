4f5ca82544eb6905bce8233d7d9efb5e9deb7e8d
Boah Lee <boahlee@hotmail.com>
2020-05-08 12:06:48 +1000

    convert project to JSX, add React-specific ESLint config

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { class: "whatever" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
+  //   return React.createElement("div", { class: "whatever" }, [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, {
+  //       name: "Luna",
+  //       animal: "Dog",
+  //       breed: "Cockatiel",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pepper",
+  //       animal: "Bird",
+  //       breed: "Cockatiel",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Doink",
+  //       animal: "Cat",
+  //       breed: "Mixed",
+  //     }),
+  //   ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
